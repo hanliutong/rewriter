5501d77f8d55310ec23b07f652e88e6324d9e5c1
Liutong HAN <liutong2020@iscas.ac.cn>
2023-05-23 21:57:33 +0800

    Support nlanes in array size.

--- checks/nlanes_check.cpp
+++ checks/nlanes_check.cpp
@@ -1,5 +1,6 @@
 #include "nlanes_check.hpp"
 #include "clang/Lex/Lexer.h"
+// #include "clang/ASTMatchers/ASTMatchFinder.h"
 
 namespace clang::tidy {
 
@@ -9,51 +10,50 @@ NlanesCheck::NlanesCheck(StringRef name, ClangTidyContext *context)
 void NlanesCheck::registerMatchers(ast_matchers::MatchFinder *finder) {
   using namespace ast_matchers;
   finder->addMatcher(
-      declRefExpr(hasDeclaration(namedDecl(matchesName("nlanes")))).bind("x"), this);
+      declRefExpr(isExpansionInMainFile(), hasDeclaration(namedDecl(hasName("nlanes"))), unless(hasAncestor(varDecl(hasType(arrayType()))))).bind("x"), this);
+  finder->addMatcher(
+      declRefExpr(isExpansionInMainFile(), hasDeclaration(namedDecl(hasName("nlanes"))), hasAncestor(varDecl(hasType(arrayType())))).bind("array"), this);
 }
-
 void NlanesCheck::check(const ast_matchers::MatchFinder::MatchResult &result) {
-  const auto *matchedExpr = result.Nodes.getNodeAs<DeclRefExpr>("x");
-  if (!matchedExpr)
-    return;  // Not found any usage of nlanes
+  const DeclRefExpr *matchedExpr = result.Nodes.getNodeAs<DeclRefExpr>("x");
+  const DeclRefExpr *arrayNlanes = result.Nodes.getNodeAs<DeclRefExpr>("array");
 
-  const auto *vecStructDecl = dyn_cast<CXXRecordDecl>(
-      matchedExpr->getDecl()->getDeclContext()->getParent());
-  if (!vecStructDecl)
-    return;  // Ignore nlanes that are not class members, e.g. `int nlanes = 8`
-
-  if (vecStructDecl->getName().compare("VTraits") == 0)
-    return;  // Already using the new API, no modification required.
-
-  if (matchedExpr->getNameInfo().getAsString().compare("nlanes") == 0 &&
-      vecStructDecl->getName().starts_with("v_")) {  // v_type::nlanes
-    SourceManager &SM = result.Context->getSourceManager();
-    std::string vecTypeName =  // get vector type from source code
-        clang::Lexer::getSourceText(
-            CharSourceRange::getTokenRange(
-                result.Context->getFullLoc(matchedExpr->getBeginLoc())),
-            SM, result.Context->getLangOpts())
-            .str();
-    auto parent = result.Context->getParents(*matchedExpr)[0];
-    if (const TypeLoc *parentTypeLoc = parent.get<TypeLoc>()) {
-      // v_type::nlanes is used as the array size,
-      // so it needs to be a compile-time constant
-      if (const ArrayTypeLoc arrTypeLoc =
-              parentTypeLoc->getAs<ArrayTypeLoc>()) {
-        diag(matchedExpr->getLocation(), "Found nlanes as array size.")
-            << FixItHint::CreateReplacement(
-                   arrTypeLoc.getSizeExpr()->getSourceRange(),
-                   "VTraits<" + vecTypeName + ">::max_nlanes");
-      }
-    } else {
+  if (matchedExpr && matchedExpr->getNameInfo().getAsString().compare("nlanes") == 0) {
+    const auto *vecStructDecl = dyn_cast<CXXRecordDecl>(matchedExpr->getDecl()->getDeclContext()->getParent());
+    if (vecStructDecl && vecStructDecl->getName().starts_with("v_")) {  // v_type::nlanes
+      SourceManager &SM = result.Context->getSourceManager();
+      std::string vecTypeName =  // get vector type from source code
+          clang::Lexer::getSourceText(
+              CharSourceRange::getTokenRange(
+                  result.Context->getFullLoc(matchedExpr->getBeginLoc())),
+              SM, result.Context->getLangOpts())
+              .str();
+      auto parent = result.Context->getParents(*matchedExpr)[0];
       diag(matchedExpr->getLocation(), "Found nlanes.")
           << FixItHint::CreateReplacement(parent.getSourceRange(),
                                           "VTraits<" + vecTypeName + ">::vlanes()");
+      // } else if (matchedExpr) {  // vecStructDecl is not start with "v_"
+      //   diag(matchedExpr->getLocation(),
+      //        "Found others nlanes, maybe not a member of the Universal Intrinsic type",
+      //        clang::DiagnosticIDs::Note);
     }
-  } else if (matchedExpr) {  // vecStructDecl is not start with "v_"
-    diag(matchedExpr->getLocation(),
-         "Found others nlanes, maybe not a member of the Universal Intrinsic type",
-         clang::DiagnosticIDs::Note);
+  }
+
+  if (arrayNlanes && arrayNlanes->getNameInfo().getAsString().compare("nlanes") == 0) {
+    const auto *vecStructDecl = dyn_cast<CXXRecordDecl>(arrayNlanes->getDecl()->getDeclContext()->getParent());
+    if (vecStructDecl && vecStructDecl->getName().starts_with("v_")) {  // v_type::nlanes
+      SourceManager &SM = result.Context->getSourceManager();
+      arrayNlanes->getSourceRange().dump(SM);
+      std::string vecTypeName =  // get vector type from source code
+          clang::Lexer::getSourceText(
+              CharSourceRange::getTokenRange(
+                  result.Context->getFullLoc(arrayNlanes->getBeginLoc())),
+              SM, result.Context->getLangOpts())
+              .str();
+      diag(arrayNlanes->getLocation(), "Found nlanes as array size.")
+          << FixItHint::CreateReplacement(arrayNlanes->getSourceRange(),
+                                          "VTraits<" + vecTypeName + ">::max_nlanes");
+    }
   }
 }
 
