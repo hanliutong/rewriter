27d7a0c51d39752d2e858f8fcdbf6ef4aa1e1ad5
Liutong HAN <liutong2020@iscas.ac.cn>
2023-05-25 16:28:05 +0800

    Update the testcase.

--- examples/overloadedOp.cpp
+++ examples/overloadedOp.cpp
@@ -35,23 +35,42 @@ void vcompare(v_float32 &a, v_float32 &b) {
   (void)res;
 }
 
-void vcompute(v_float32 &a, v_float32 &b) {
+void vcompute(v_float32 &a, v_float32 &b, v_float32 &c, v_float32 &d) {
   v_float32 res = a + b;
   res = a - b;
   res = a * b;
   res = a / b;
   res = res + a + b;
   res = res * a * b;
-  res = res + a - b;
+  res = a + b - (c + d);
+  res = (a + b) - (c + d);
+  res = a + b * c - d;
+  res = a + b * (c - a) / d;
+  res += a;
+  res -= b;
+  res *= c;
+  res /= d;
 
   (void)res;
 }
 
+void vshift(v_uint32 &a) {
+  a << 5;
+  a >> 10;
+}
+
 void vlogic(v_uint16 &a, v_uint16 &b) {
   v_uint16 res = a & b;
+  res = res & a & b;
   res = a | b;
+  res = res | a | b;
   res = a ^ b;
+  res = res ^ a ^ b;
   res = ~res;
+  res = res | (~a) & b;
+  res &= a;
+  res |= a;
+  res ^= a;
 
   (void)res;
 }
